9db61d6d4ae0a395adcfa6aa9be014cf073f10ef
Jay Hayes <ur@iamvery.com>
2018-12-03 08:46:12 -0600

    wip

--- elm/day1/src/Calibration.elm
+++ elm/day1/src/Calibration.elm
@@ -1,5 +1,59 @@
 module Calibration exposing (..)
 
+import Iter
+import Set
+
 
 calculate =
     List.foldl (+) 0
+
+
+remove element list =
+    case list of
+        [] ->
+            list
+
+        e :: l ->
+            if e == element then
+                l
+            else
+                e :: (remove element l)
+
+
+difference l1 l2 =
+    List.foldl remove l1 l2
+
+
+duplicates list =
+    difference list (Set.fromList list |> Set.toList)
+
+
+makeIter st f =
+    Iter.It { state = st, next = f }
+
+
+dropWhile pred (Iter.It it) =
+    makeIter it.state
+        (\s ->
+            case it.next s of
+                Just ( s_, x ) ->
+                    if pred x then
+                        Nothing
+                    else
+                        Just ( s_, x )
+
+                Nothing ->
+                    Nothing
+        )
+
+
+calibrate changes =
+    Iter.cycle changes
+        |> Iter.accumulate (+) 0
+        |> Iter.accumulate (::) []
+        |> Iter.map duplicates
+        -- find duplicates in each list here!
+        --|> dropWhile List.isEmpty
+        -- filter on non-empty lists, first one is our match!
+        |> Iter.take 10
+        |> Iter.toList
--- elm/day1/tests/Example.elm
+++ elm/day1/tests/Example.elm
@@ -14,4 +14,18 @@ suite =
             \_ -> Calibration.calculate ([ 1, 1, -2 ]) |> Expect.equal 0
         , test "example 3" <|
             \_ -> Calibration.calculate ([ -1, -2, -3 ]) |> Expect.equal -6
+        , describe "calibrate"
+            [ test "example 1" <|
+                \_ -> Calibration.calibrate [ 1, -2, 3, 1 ] |> Expect.equal [ [] ]
+            ]
+        , describe "difference"
+            [ test "returns items in first list after second list is removed" <|
+                \_ -> Calibration.difference [ 1, 2 ] [ 2 ] |> Expect.equal [ 1 ]
+            ]
+        , describe "duplicates"
+            [ test "none" <|
+                \_ -> Calibration.duplicates [ 1, 2, 3 ] |> Expect.equal []
+            , test "one" <|
+                \_ -> Calibration.duplicates [ 1, 2, 1 ] |> Expect.equal [ 1 ]
+            ]
         ]
