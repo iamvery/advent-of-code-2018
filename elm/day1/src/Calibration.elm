90b8ca60e8e650c5f8e0c629593d3994bb4e8ac4
Jay Hayes <ur@iamvery.com>
2018-12-03 09:33:32 -0600

    d1p2: Retinker calibrate function with new Iter.Extra.dropWhile

--- elm/day1/src/Calibration.elm
+++ elm/day1/src/Calibration.elm
@@ -1,6 +1,8 @@
 module Calibration exposing (..)
 
 import Iter
+import Iter.Extra
+import List.More
 import Set
 
 
@@ -8,52 +10,16 @@ calculate =
     List.foldl (+) 0
 
 
-remove element list =
-    case list of
-        [] ->
-            list
-
-        e :: l ->
-            if e == element then
-                l
-            else
-                e :: (remove element l)
-
-
-difference l1 l2 =
-    List.foldl remove l1 l2
-
-
-duplicates list =
-    difference list (Set.fromList list |> Set.toList)
-
-
-makeIter st f =
-    Iter.It { state = st, next = f }
-
-
-dropWhile pred (Iter.It it) =
-    makeIter it.state
-        (\s ->
-            case it.next s of
-                Just ( s_, x ) ->
-                    if pred x then
-                        Nothing
-                    else
-                        Just ( s_, x )
-
-                Nothing ->
-                    Nothing
-        )
-
-
 calibrate changes =
-    Iter.cycle changes
+    changes
+        |> Iter.cycle
         |> Iter.accumulate (+) 0
-        |> Iter.accumulate (::) []
-        |> Iter.map duplicates
-        -- find duplicates in each list here!
-        --|> dropWhile List.isEmpty
-        -- filter on non-empty lists, first one is our match!
-        |> Iter.take 10
+        |> Iter.accumulate (::) [ 0 ]
+        |> Iter.map List.More.duplicates
+        |> Iter.Extra.dropWhile List.isEmpty
+        |> Iter.take 1
         |> Iter.toList
+        |> List.head
+        |> Maybe.withDefault []
+        |> List.head
+        |> Maybe.withDefault 0
--- elm/day1/tests/Example.elm
+++ elm/day1/tests/Example.elm
@@ -16,16 +16,14 @@ suite =
             \_ -> Calibration.calculate ([ -1, -2, -3 ]) |> Expect.equal -6
         , describe "calibrate"
             [ test "example 1" <|
-                \_ -> Calibration.calibrate [ 1, -2, 3, 1 ] |> Expect.equal [ [] ]
-            ]
-        , describe "difference"
-            [ test "returns items in first list after second list is removed" <|
-                \_ -> Calibration.difference [ 1, 2 ] [ 2 ] |> Expect.equal [ 1 ]
-            ]
-        , describe "duplicates"
-            [ test "none" <|
-                \_ -> Calibration.duplicates [ 1, 2, 3 ] |> Expect.equal []
-            , test "one" <|
-                \_ -> Calibration.duplicates [ 1, 2, 1 ] |> Expect.equal [ 1 ]
+                \_ -> Calibration.calibrate [ 1, -2, 3, 1 ] |> Expect.equal 2
+            , test "example 2" <|
+                \_ -> Calibration.calibrate [ 1, -1 ] |> Expect.equal 0
+            , test "example 3" <|
+                \_ -> Calibration.calibrate [ 3, 3, 4, -2, -4 ] |> Expect.equal 10
+            , test "example 4" <|
+                \_ -> Calibration.calibrate [ -6, 3, 8, 5, -6 ] |> Expect.equal 5
+            , test "example 5" <|
+                \_ -> Calibration.calibrate [ 7, 7, -2, -7, -4 ] |> Expect.equal 14
             ]
         ]
