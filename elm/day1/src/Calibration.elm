42ded82894df57ca492cc7e2429c48ec86d9811e
Jay Hayes <ur@iamvery.com>
2018-12-04 15:02:18 -0600

    d1p2: Finally solve part 2 with recursive stream

--- elm/day1/src/Calibration.elm
+++ elm/day1/src/Calibration.elm
@@ -1,5 +1,40 @@
 module Calibration exposing (..)
 
+import Iter
+
 
 calculate =
     List.foldl (+) 0
+
+
+type alias Frequency =
+    Int
+
+
+type alias Changes =
+    List Frequency
+
+
+calibrate : Changes -> Frequency
+calibrate changes =
+    changes
+        |> Iter.cycle
+        |> calibrate_ ( [], 0 )
+        |> Tuple.second
+
+
+calibrate_ : ( Changes, Frequency ) -> Iter.Iter Frequency (List Frequency) -> ( Changes, Frequency )
+calibrate_ ( frequencies, frequency ) changes =
+    case Iter.step changes of
+        ( _, Nothing ) ->
+            ( frequencies, frequency )
+
+        ( nextChanges, Just change ) ->
+            let
+                newFrequency =
+                    frequency + change
+            in
+                if List.member frequency frequencies then
+                    ( frequencies, frequency )
+                else
+                    calibrate_ ( frequency :: frequencies, newFrequency ) nextChanges
--- elm/day1/tests/Example.elm
+++ elm/day1/tests/Example.elm
@@ -14,4 +14,16 @@ suite =
             \_ -> Calibration.calculate ([ 1, 1, -2 ]) |> Expect.equal 0
         , test "example 3" <|
             \_ -> Calibration.calculate ([ -1, -2, -3 ]) |> Expect.equal -6
+        , describe "calibrate"
+            [ test "example 1" <|
+                \_ -> Calibration.calibrate [ 1, -2, 3, 1 ] |> Expect.equal 2
+            , test "example 2" <|
+                \_ -> Calibration.calibrate [ 1, -1 ] |> Expect.equal 0
+            , test "example 3" <|
+                \_ -> Calibration.calibrate [ 3, 3, 4, -2, -4 ] |> Expect.equal 10
+            , test "example 4" <|
+                \_ -> Calibration.calibrate [ -6, 3, 8, 5, -6 ] |> Expect.equal 5
+            , test "example 5" <|
+                \_ -> Calibration.calibrate [ 7, 7, -2, -7, -4 ] |> Expect.equal 14
+            ]
         ]
