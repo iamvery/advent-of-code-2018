a46c724d09ec9d0521db47deb29e7f49c8e90575
Jay Hayes <ur@iamvery.com>
2018-12-05 17:06:55 -0600

    d2p1: First solution. Seems... complicated :D

--- elm/day2/src/InventoryManagementSystem.elm
+++ elm/day2/src/InventoryManagementSystem.elm
@@ -1,6 +1,54 @@
 module InventoryManagementSystem exposing (..)
 
+import List.Extra
+
 
 checksum : List String -> number
-checksum _ =
-    0
+checksum list =
+    case List.foldl sumMeansurements ( 0, 0 ) list of
+        ( x, y ) ->
+            x * y
+
+
+sumMeansurements candidate ( x, y ) =
+    let
+        g =
+            group candidate
+    in
+        ( x + (measureDouble g), y + (measureTriple g) )
+
+
+type alias ParsedCandidate =
+    List ( Char, List Char )
+
+
+group : String -> ParsedCandidate
+group candidate =
+    candidate
+        |> String.toList
+        |> List.Extra.gatherEquals
+
+
+measureDouble : ParsedCandidate -> number
+measureDouble =
+    measure 2
+
+
+measureTriple : ParsedCandidate -> number
+measureTriple =
+    measure 3
+
+
+measure : Int -> ParsedCandidate -> number
+measure count candidate =
+    let
+        remainingCount =
+            count - 1
+
+        check =
+            \( _, remaining ) -> List.length remaining == remainingCount
+    in
+        if List.any check candidate then
+            1
+        else
+            0
--- elm/day2/tests/Example.elm
+++ elm/day2/tests/Example.elm
@@ -14,4 +14,20 @@ suite =
             [ test "example" <|
                 \_ -> InventoryManagementSystem.checksum Input.example |> Expect.equal 12
             ]
+        , describe "group"
+            [ test "candidate with double and triple" <|
+                \_ ->
+                    InventoryManagementSystem.group "bababc"
+                        |> Expect.equal
+                            [ ( 'b', [ 'b', 'b' ] )
+                            , ( 'a', [ 'a' ] )
+                            , ( 'c', [] )
+                            ]
+            ]
+        , describe "measureDouble"
+            [ test "group with double" <|
+                \_ -> InventoryManagementSystem.measureDouble [ ( 'a', [ 'a' ] ) ] |> Expect.equal 1
+            , test "group with triple" <|
+                \_ -> InventoryManagementSystem.measureDouble [ ( 'a', [ 'a', 'a' ] ) ] |> Expect.equal 0
+            ]
         ]
